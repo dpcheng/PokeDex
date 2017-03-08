c01df895266dfd8052a79f73b7e0584cb60de0d1
Dylan <dylan.patrick.cheng@gmail.com>
2017-03-08 14:08:31 -0800

    complete phase2, pokemon redux cycle

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import { requestAllPokemon } from './actions/pokemon_actions';
+import { selectAllPokemon } from './reducers/selectors';
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>POKEMONS</h1>, root);
+  window.store = store;
+  window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
 });
